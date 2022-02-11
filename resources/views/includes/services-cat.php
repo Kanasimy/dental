cfaf1dfd1e468817d788ced4736cda4832cfc2d3
Kanasimy <admin@active-way.ru>
2022-02-11 17:11:36 +0700

    redesign of the service category page

--- resources/views/includes/services-cat.php
+++ resources/views/includes/services-cat.php
@@ -5,7 +5,7 @@ if (!function_exists('post_categories')) {
    */
   function services_cat()
   { $slug = get_queried_object()->slug;
-    echo do_shortcode('[ajax_load_more no_results_text="Больше услуг нет" loading_style="white" container_type="ul" button_label="Показать еще" transition_container="false" scroll="false" post_type="service"  taxonomy="services" taxonomy_terms="'.$slug.'" taxonomy_operator="IN"]');
+    echo do_shortcode('[ajax_load_more posts_per_page="50" no_results_text="Больше услуг нет" loading_style="white" container_type="ul" button_label="Показать еще" transition_container="false" scroll="false" post_type="service"  taxonomy="services" taxonomy_terms="'.$slug.'" taxonomy_operator="IN"]');
   }
 
 }
